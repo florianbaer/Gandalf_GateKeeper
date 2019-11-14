00ab0bf9d11cd78905a0d5ca63b36e02dbe1cf2f
florianbaer <florianbaer@bluewin.ch>
2019-11-14 21:54:16 +0100

    Implement first simple face detection

--- Project/states/recognizing_face.py
+++ Project/states/recognizing_face.py
@@ -5,20 +5,35 @@ import time
 def recognizing_face(gandalf):
     faceDetectedMemValue = "FaceDetected"
 
-    say = "recognizing face"
-    logging.info(say)
+    logging.info("recognizing face")
     gandalf.robot.ALFaceDetection.subscribe("Test_Face", 2000, 0.0)
 
     val = gandalf.robot.ALMemory.getData(faceDetectedMemValue)
+    name = None
     # A simple loop that reads the memValue and checks whether faces are detected.
     while not (val and isinstance(val, list) and len(val) >= 2):
         time.sleep(0.5)
         val = gandalf.robot.ALMemory.getData(faceDetectedMemValue)
+        name = _face_already_known(gandalf, val)
+
     gandalf.face_in_front = True
     # Check whether we got a valid output.
     logging.info("face found")
     gandalf.robot.ALFaceDetection.unsubscribe("Test_Face")
+    if name is not None:
+        gandalf.robot.ALAnimatedSpeech.say('I know you {}'.format(val[1][1][1][0]))
     gandalf.trigger("question_intention")
 
 
+def _face_already_known(gandalf, val):
+    faces = gandalf.robot.ALFaceDetection.getLearnedFacesList()
+    logging.debug(faces)
+    if (val and isinstance(val, list) and len(val) >= 2 and len(val[1][1]) > 0):
+        logging.debug(val[1][1][1][0])
+        return val[1][1][1][0]
+    # gandalf.robot.ALFaceDetection.reLearnFace("Peter")
+    faces = gandalf.robot.ALFaceDetection.getLearnedFacesList()
+    logging.debug(faces)
+    logging.debug(val)
+    return None
 
--- Project/states/starting_up.py
+++ Project/states/starting_up.py
@@ -3,5 +3,5 @@ import logging
 def starting_up(gandalf):
     say = "Starting up"
     logging.info(say)
-    gandalf.robot.ALAnimatedSpeech.say(say)
+    #gandalf.robot.ALAnimatedSpeech.say(say)
     gandalf.trigger('initializing')
