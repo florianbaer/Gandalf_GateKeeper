bccd6d4434a388a11021d36ff4e67ded8169761e
florianbaer <florianbaer@bluewin.ch>
2019-12-06 16:31:13 +0100

    Work on move back

--- Project/states/move_to_side.py
+++ Project/states/move_to_side.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from math import pi
 
 
@@ -6,6 +7,10 @@ def move_to_side(gandalf):
     say = "Move to side"
     logging.info(say)
 
+
+
+
+    # for some reason almath is not available..
     gandalf.robot.ALMotion.moveTo(0, 0.4, -90 * pi / 180)
 
     # trigger next state
