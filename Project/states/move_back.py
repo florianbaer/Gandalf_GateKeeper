8e5ebfe08f563cd1922f44830f92b9d4584e2258
florianbaer <florianbaer@bluewin.ch>
2019-12-06 17:51:01 +0100

    Finalize

--- Project/states/move_back.py
+++ Project/states/move_back.py
@@ -6,13 +6,19 @@ gotit = False
 def move_back(gandalf):
     say = "Move back"
     val = []
+    person_passed = False
     logging.debug(say)
-    while len(val) == 0:
-        gandalf.robot.ALMemory.subscribeToEvent("EngagementZones","PersonEnteredZone1", "callback")
+    while len(val) != 0 or not person_passed:
+        # magic event handling
+        gandalf.robot.ALEngagementZones.subscribe("EngagementZones/PeopleInZone1", 200,200)
+        gandalf.robot.ALMemory.subscribeToEvent("EngagementZones","PeopleInZone1", "callback")
         val = gandalf.robot.ALMemory.getData("EngagementZones/PeopleInZone1")
-        logging.debug(val)
         time.sleep(2)
-        gandalf.robot.ALMemory.unsubscribeToEvent("EngagementZones","PersonEnteredZone1")
+        logging.debug(val)
+        if len(val) > 0:
+            person_passed = True
+        gandalf.robot.ALMemory.unsubscribeToEvent("EngagementZones","PeopleInZone1")
+        gandalf.robot.ALEngagementZones.unsubscribe("EngagementZones/PeopleInZone1")
 
     gandalf.robot.ALMotion.moveTo(0.4, 0, 90 * pi / 180)
 
--- Project/states/move_to_side.py
+++ Project/states/move_to_side.py
@@ -11,14 +11,17 @@ def move_to_side(gandalf):
     gandalf.robot.ALLaser.setDetectingLength(20, 3000)
     gandalf.robot.ALLaser.setOpeningAngle(1.0, 1.0)
 
-    lef_side_y = 0
+    lef_side_y = gandalf.robot.ALMemory.getData(
+        "Device/SubDeviceList/Platform/LaserSensor/Left/Horizontal/Seg01/Y/Sensor/Value")
 
     while lef_side_y < 0.5:
         lef_side_y = gandalf.robot.ALMemory.getData("Device/SubDeviceList/Platform/LaserSensor/Left/Horizontal/Seg01/Y/Sensor/Value")
         gandalf.robot.ALAnimatedSpeech.say("There's an obstacle to my left, move me to my right please.")
         time.sleep(2)
-
+    # for some reason almath is not available..
+    gandalf.robot.ALMotion.moveTo(0, 0.4, -90 * pi / 180)
     gandalf.robot.ALLaser.laserOFF()
+    gandalf.trigger('move_back')
 
 
 
