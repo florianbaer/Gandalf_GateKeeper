b7fdab7d7788147753e07582a7f9daaf4c281ba4
florianbaer <florianbaer@bluewin.ch>
2019-12-13 10:38:33 +0100

    Merge branch 'develop' of gitlab.enterpriselab.ch:RobLab/roblab-19hs/group8 into develop
    # Conflicts:
    #	Project/states/move_to_side.py

--- Project/states/move_back.py
+++ Project/states/move_back.py
@@ -8,21 +8,24 @@ def move_back(gandalf):
     person_passed = False
     logging.debug(say)
 
+    gandalf.robot.ALPeoplePerception.subscribe("ALPeoplePerception", 2, 5)
+    gandalf.robot.ALEngagementZones.subscribe("ALEngagementZones", 2, 5)
+
     # as long as nobody passed pepper, it shall wait
     while len(val) != 0 or not person_passed:
-        # magic event handling
-        gandalf.robot.ALEngagementZones.subscribe("EngagementZones/PeopleInZone1", 200, 200)
-        gandalf.robot.ALMemory.subscribeToEvent("EngagementZones","PeopleInZone1", "callback")
+
         val = gandalf.robot.ALMemory.getData("EngagementZones/PeopleInZone1")
-        time.sleep(2)
+        time.sleep(0.5)
         logging.debug(val)
         if len(val) > 0:
             person_passed = True
-        gandalf.robot.ALMemory.unsubscribeToEvent("EngagementZones","PeopleInZone1")
-        gandalf.robot.ALEngagementZones.unsubscribe("EngagementZones/PeopleInZone1")
-
+    gandalf.robot.ALPeoplePerception.unsubscribe("ALPeoplePerception")
+    gandalf.robot.ALEngagementZones.unsubscribe("ALEngagementZones")
     gandalf.robot.ALMotion.moveTo(0.4, 0, 90 * pi / 180)
 
     # trigger starting state
     gandalf.trigger('re_initialize')
 
+def callback():
+    name, id, subId = 1
+    logging.warning("OH nooooo {}{}{}",  name, id, subId)
